f2b97258de13a72b6712cb1e2d9604abfa5f2cc8
Vadim Balichev <VadimBalychevJ@yandex.ru>
2023-05-03 00:14:13 +0300

    коммит

--- src/main/java/project/control/model/enums/Phase.java
+++ src/main/java/project/control/model/enums/Phase.java
@@ -1,12 +1,8 @@
 package project.control.model.enums;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
-@AllArgsConstructor
 public enum Phase {
     PLANNED,
     IN_PROCESS,
